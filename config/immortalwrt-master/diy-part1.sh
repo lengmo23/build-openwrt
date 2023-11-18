4eca0d07f5ce3d6395d959d027e596d02192bd74
lengmo23 <77873203+lengmo23@users.noreply.github.com>
2023-11-19 01:12:00 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -6,9 +6,14 @@
 # Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
 #========================================================================================================================
 
+
+
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
+sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
+#src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci  //如果是编译passwall2着不需要否则会加载重复软件包
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' feeds.conf.default
+sed -i '$a src-git smallpackages https://github.com/kenzok8/small-package.git' feeds.conf.default
 
