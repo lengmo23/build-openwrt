62d4c0526b8b7fb27e0b3ed7262cb097ed18ec42
lengmo23 <77873203+lengmo23@users.noreply.github.com>
2024-01-28 16:45:48 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -34,7 +34,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-#svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
 
 
