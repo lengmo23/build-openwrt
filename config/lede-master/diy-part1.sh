324b9bcf8c9d27c42d8b18606878ee2911df80c0
lengmo23 <77873203+lengmo23@users.noreply.github.com>
2024-01-28 16:15:15 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,10 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' feeds.conf.default
-sed -i '$a src-git smallpackages https://github.com/kenzok8/small-package.git' feeds.conf.default
-
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
